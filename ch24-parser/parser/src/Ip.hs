6465b74d7a1e4d8ccd8a4c0416f6f2c292958e9b
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-07-07 11:11:03 +0300

    IPv6

--- ch24-parser/parser/src/Ip.hs
+++ ch24-parser/parser/src/Ip.hs
@@ -5,7 +5,11 @@ module Ip where
 import Control.Applicative
 import Data.Bits ((.|.), shiftL)
 import qualified Data.ByteString as BS
+import qualified Data.List as L
+import Data.List
 import Data.Word
+import Debug.Trace
+import Numeric (readHex)
 import Text.Trifecta
 
 data IPv4 = IPv4 Word32 deriving (Eq, Show)
@@ -22,14 +26,39 @@ ipv4Parcer = IPv4 <$> conv <$> parseIpv4Parts
     conv :: [Word8] -> Word32
     conv = foldl (\acc b -> (acc `shiftL` 8) .|. (fromIntegral b)) 0
 
-
-
-data IPAddress6 =
-  IPAddress6 Word64 Word64
+data IPv6
+  = IPv6 Word64 Word64
   deriving (Eq, Ord, Show)
 
+parseIpv6Part :: Parser Word16
+parseIpv6Part = do
+  res <- manyTill hexDigit (colon *> return () <|> eof)
+  return $ case res of
+    [] -> 0
+    xs ->
+      fst . head $ readHex (xs)
+
+parseIpv6Parts :: Parser [Word16]
+parseIpv6Parts = count 8 (parseIpv6Part)
+
+parseIpv6 :: Parser IPv6
+parseIpv6 = do
+  (a, b) <- conv <$> parseIpv6Parts
+  return $ IPv6 a b
+  where
+    conv parts =
+      let (a, b) = splitAt 4 parts
+       in (toWord64 a, toWord64 b)
+    toWord64 =
+      foldl (\acc p -> acc `shiftL` 16 .|. (fromIntegral p)) 0
+
+readPart p = parseString parseIpv6Part mempty p
 
 main :: IO ()
 main = do
   print $ parseString ipv4Parcer mempty "172.16.254.1"
-
+  print $
+    parseString
+      parseIpv6
+      mempty
+      "2001:DB8::8:800:200C:417A"
